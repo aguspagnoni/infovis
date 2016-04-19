e478264b88e287dc369585564340adb8c2e9da96
noeliablopez <noelia.lopez@wolox.com.ar>
2016-04-19 01:04:47 -0300

    Bar chart | Personal data

--- scripts/datos_personales.js
+++ scripts/datos_personales.js
@@ -4,6 +4,10 @@ function finances() {
 
   var dataset = [];
 
+  var width = 1200;
+  var height = 300;
+  var barPadding = 2;
+
   $('tr').each(function(i) {
 
     dataset.push({
@@ -14,27 +18,31 @@ function finances() {
 
   });
 
-  var amounts = dataset.map(function(d) {return d.amount});
   var scale = d3.scale.linear()
-                  .domain([0, 5000])
-                  .range([100, 0]);
+                  .domain([0, d3.max(dataset, function(d) { return d.amount} )])
+                  .range([200, 0]);
 
+  var color = d3.scale.quantize()
+              .domain([0, d3.max(dataset, function(d) { return d.amount} ) / 2, d3.max(dataset, function(d) { return d.amount} )])
+              .range(["#FF5335", "#FFD300", "#80EF91"]);
 
   var svgContainer = d3.select("#ej-ctn").append("svg")
-                                           .attr("width", 800)
-                                           .attr("height", 600);
+                                           .attr("width", width)
+                                           .attr("height", height);
 
 
     svgContainer.selectAll('expense')
                 .data(dataset)
                 .enter()
                 .append('rect')
-                .attr("fill", "#80EF91")
-                .attr("x", function(d, i) { return 5 + i*20})
-                .attr("y", 20)
+                .attr("fill", function(d) { return color(d.amount); })
+                .attr("x", function(d, i) {return i * (width / dataset.length); })
+                .attr("y", function(d) { return height - scale(d.amount); })
                 .style("display", "inline-block")
-                .style("width", '10px')
-                .style("height", function(d) { return scale(d.amount) + 'px' });
+                .transition()
+                .duration(300)
+                .attr("width", width / dataset.length - barPadding)
+                .style("height", function(d) { return scale(d.amount)});
 
 
 
